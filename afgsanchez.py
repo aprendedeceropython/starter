b2a31566bc94fb80e9e5b2a1b4c34342e4e4a9c2
afgsanchez <afgsanchez@gmail.com>
2024-04-28 22:22:26 +0200

    practica starter v 1.0.1

---
+++ afgsanchez.py
@@ -0,0 +1,47 @@
+# variables tipo integer (enteros)
+entero = 5
+entero1 = 6
+
+# variable tipo float (flotantes, numeros con decimal)
+flotante = 3.14
+flotante1 = 1.26
+
+# variable tipo string (cadenas de texto)
+texto = "Me llamo Toni."
+texto2 = "Encantado de conoceros"
+
+# variable tipo booleano (True o False)
+boleano = True
+
+# variables tipo listas (Almacenan un serie de elementos)
+lista_enteros = [1, 2, 3, 4]
+lista_texto = ['uno', 'dos', 'tres', 'cuatro']
+
+# variables tipo tupla (son listas inmutables, que
+# no se puede cambiar el contenido)
+tupla = (1, 2, 3, 4)
+
+# variables tipo diccionario (listas que almacenan
+# pares)
+diccionario = {"Nombre": "Toni", "Localidad": "Baleares"}
+
+# variables tipo set (almacenan colecciones de datos
+# no ordenados y no repetidos)
+variable_set = {"rojo", "azul", "amarillo", "verde"}
+
+#------------------------------#
+# Ejercicios con las variables #
+#------------------------------#
+
+# Concatena variables de tipo texto.
+print(texto, texto2)
+print()
+
+# Haz las diferentes operaciones con variable numéricas (Suma, Resta, Multiplicación, División, Residuo, Potenciación).
+# Suma enteros
+print(entero + entero1, " - Suma de integer")
+# Suma entero + flotante
+f = entero + flotante1
+print(f, " - Suma de integer + float")
+
+
